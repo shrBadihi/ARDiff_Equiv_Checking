feb7b318b00badbb3265bd561ed9dac6a68e785c
farid <faridath.akinotcho@yahoo.fr>
2020-09-07 11:56:40 -0700

    code: update wrong z3 path in Paths, pointing to deleted Libraries/MacBuild

--- Implementation/src/main/java/equiv/checking/Paths.java
+++ Implementation/src/main/java/equiv/checking/Paths.java
@@ -16,6 +16,6 @@ public final class Paths {
     public static String sourcepath="src/examples/demo/instrumented";
     public static final String jpf_symbc="./jpf-git/jpf-symbc";
     public static final String jpf_core="./jpf-git/jpf-core";
-    public static String z3 = "./Libraries/z3MacBuild/";
+    public static String z3 = "z3";
     public static String dp = "./jpf-git/jpf-symbc/lib/";
 }
